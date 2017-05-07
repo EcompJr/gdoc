131f09dac74d299057e2bfe0644ad8191c9362ca
Lucas Alves <lucas2213690@gmail.com>
2017-05-07 18:53:50 -0300

    Adicionado Cadastro de Novos Usuários

---
+++ assets/js/cadastroNovosUsuarios.js
@@ -0,0 +1,42 @@
+		$(document).ready(function () {
+		
+			$.getJSON('https://gist.githubusercontent.com/ografael/2037135/raw/5d31e7baaddd0d599b64c3ec04827fc244333447/estados_cidades.json', function (data) {
+				var items = [];
+				var options = '<option value="">Escolha um Estado</option>';	
+				$.each(data, function (key, val) {
+					options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+				});					
+				$("#estados").html(options);				
+				
+				$("#estados").change(function () {				
+				
+					var options_cidades = '';
+					var str = "";					
+					
+					$("#estados option:selected").each(function () {
+						str += $(this).text();
+					});
+					
+					$.each(data, function (key, val) {
+						if(val.nome == str) {							
+							$.each(val.cidades, function (key_city, val_city) {
+								options_cidades += '<option value="' + val_city + '">' + val_city + '</option>';
+							});							
+						}
+					});
+					$("#cidades").html(options_cidades);
+					
+				}).change();		
+			
+			});
+
+            $.getJSON('https://raw.githubusercontent.com/juliolvfilho/lista-paises/master/paises-array.json', function (data) {
+				var items = [];
+				var options = '<option placeholder="Países" value=""></option>';	
+				$.each(data, function (key, val) {
+					options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+				});
+                $("#pais").html(options);
+            });
+		
+		});
