911e6f69b9956d1deb7f7b7292b5654b1d289d03
Lucas <lucas2213690@gmail.com>
2017-05-09 17:45:01 -0300

    Melhorias gerais II

---
+++ assets/js/manutencaoArquivo.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+
+           var quill = new Quill('#editor', {
+            placeholder: 'Escreva o documento aqui',
+            theme: 'bubble'
+        });
+
+});
