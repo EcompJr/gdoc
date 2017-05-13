8a329ad220e5f8ca18834236fb4e7364d68aa2d1
Lucas Alves <lucas2213690@gmail.com>
2017-05-12 22:34:31 -0300

    Pequenos Ajustes

--- assets/js/manutencaoArquivo.js
+++ assets/js/manutencaoArquivo.js
@@ -1,9 +1,22 @@
 
 $(document).ready(function () {
 
+
+            var toolbarOptions = [
+                        [{ 'font': [] }],
+                        ['bold', 'italic', 'underline'],
+                        ['blockquote', 'code-block'],
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                        [{ 'align': [] }],
+                        ['omega']
+                        ];
+
            var quill = new Quill('#editor', {
+            modules: {
+                toolbar: toolbarOptions
+            },
             placeholder: 'Escreva o documento aqui',
-            theme: 'bubble'
+            theme: 'snow'
         });
 
 });
