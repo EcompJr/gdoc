53e64686bdff4a67eb4354deed79f28e7042000a
Lucas Alves <lucas2213690@gmail.com>
2017-05-20 11:38:43 -0300

    Melhorias no Cadastro de Instancia

--- assets/js/cadastroInstancia.js
+++ assets/js/cadastroInstancia.js
@@ -1,23 +1,48 @@
 $(document).ready(function() {
-        var campos_max          = 10;   //max de 10 campos
+
         var x = 1; // campos iniciais
-        $('#add_field').click (function(e) {
-                e.preventDefault();     //prevenir novos clicks
+        const campos_max = 6;
+        const campos_min = 1;
+        
+        $( "#add_field" ).click(function() {
                 if (x < campos_max) {
-                        $('#listas').append('<<div class="input-group">\
-                            <div class="col-md-4">\
-                                <label for="anexar">Instâncias:</label>\
-                                <input requierd type="text" class="form-control" id="anexar">\
-                            </div>\
-                            <div class="col-md-4">\
-                                <label for="anexar">Prazo:</label>\
-                                <input requierd type="number" class="form-control" id="anexar" placeholder="Em dias">\
-                            </div>\
-                            <div class="col-md-4">\
-                                <label for="anexar">Responsável:</label>\
-                                <input requierd type="text" class="form-control" id="anexar">\
-                        </div>'0);
-                        x++;
+                        $('#listas').append('<div class="row" id="'+ ++x +'">\
+                        <div class="form-group col-md-4">\
+                            <label for="anexar">Instâncias:</label>\
+                            <input requierd type="text" class="form-control" id="anexar">\
+                        </div>\
+                        <div class="form-group col-md-4">\
+                            <label for="anexar">Prazo:</label>\
+                            <input requierd type="number" class="form-control" id="anexar" placeholder="Em dias">\
+                        </div>\
+                        <div class="form-group col-md-4">\
+                            <label for="anexar">Responsável:</label>\
+                            <select  type="text" class="form-control">\
+                                <option value="" selected>Selecione o funcionário</option>\
+                            </select>\
+                        </div>\
+                    </div>');
+
+                    //x++;
+                    
                 }
+                else{
+                     alert("Número máximo de Instâncias");   
+                }
+                
+        });
+
+        $("#remove_field").click(function(){
+
+                var id = "#" + x
+
+                if(x > campos_min){
+                        $(id).remove();
+                        x--;
+                }
+                else{
+                        alert("Número mínimo de Instâncias");
+                }
+                
         });
 });
