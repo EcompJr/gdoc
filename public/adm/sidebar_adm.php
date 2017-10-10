093720316150f7b5c059898beaa476edb5df2a8e
Gustavo Boanerges <g.boanerges2@gmail.com>
2017-10-10 12:46:06 -0300

    Add custom sidebar style

--- public/adm/sidebar_adm.php
+++ public/adm/sidebar_adm.php
@@ -4,59 +4,59 @@
         <div class="sidebar-collapse">
           <ul class="nav" id="main-menu">
   
-            <li class="active-link scrollMenu"><a aria-hidden="true" href="#">Cadastro</a></li>
+            <li id="menuId" class="active-link scrollMenu"><a aria-hidden="true" href="#">Cadastro</a></li>
             <div style="max-height:0;overflow: hidden;transition:max-height 0.2s ease-out;" class="sidebar-collapse">
               <ul style="margin-top:-50px;" class="nav">
                 <li class="active-link">
-                  <a href="cadastroNovosUsuarios.php" aria-hidden="true">Cadastro de Usuários</a>
+                  <a id="subMenuId" href="cadastroNovosUsuarios.php" aria-hidden="true">Cadastro de Usuários</a>
                 </li>
   
                 <li class="active-link">
-                  <a href="cadastroInstancias.php">Cadastro de Instâncias</a>
+                  <a id="subMenuId" href="cadastroInstancias.php">Cadastro de Instâncias</a>
                 </li>
   
                 <li class="active-link">
-                  <a href="cadastroInstrumento.php">Cadastro de Instrumentos</a>
+                  <a id="subMenuId" href="cadastroInstrumento.php">Cadastro de Instrumentos</a>
                 </li>
               </ul>
             </div>
   
-            <li class="active-link scrollMenu"><a aria-hidden="true" href="#">Consulta</a></li>
+            <li id="menuId" class="active-link scrollMenu"><a aria-hidden="true" href="#">Consulta</a></li>
             <div class="sidebar-collapse" style="max-height:0;overflow: hidden;transition:max-height 0.2s ease-out;">
               <ul style="margin-top:-50px;" class="nav">
                 <li class="active-link">
-                  <a href="movimentacaoProcesso.php">Movimentação de Processos</a>
+                  <a id="subMenuId" href="movimentacaoProcesso.php">Movimentação de Processos</a>
                 </li>
   
                 <li class="active-link">
-                  <a href="#">Consulta de Protocolo</a>
+                  <a id="subMenuId" href="#">Consulta de Protocolo</a>
                 </li>
                 <li class="active-link">
-                  <a href="#">Consulta de Movimentação</a>
+                  <a id="subMenuId" href="#">Consulta de Movimentação</a>
                 </li>
               </ul>
             </div>
   
-            <li class="active-link scrollMenu"><a aria-hidden="true" href="#">Arquivo</a></li>
+            <li id="menuId" class="active-link scrollMenu"><a aria-hidden="true" href="#">Arquivo</a></li>
             <div class="sidebar-collapse" style="max-height:223px;overflow: hidden;transition:max-height 0.2s ease-out;">
               <ul style="margin-top:-50px;" class="nav">
                 <li class="active-link">
-                  <a href="cadastroArquivo.php">Cadastro de Arquivo</a>
+                  <a id="subMenuId" href="cadastroArquivo.php">Cadastro de Arquivo</a>
                 </li>
                 <li class="active-link">
-                  <a href="manutencaoArquivo.php">Abertura de Arquivo</a>
+                  <a id="subMenuId" href="manutencaoArquivo.php">Abertura de Arquivo</a>
                 </li>
               </ul>
             </div>
   
-            <li class="active-link">
+            <li id="menuId" class="active-link">
               <a href="relatorio.php">Relatório</a>
             </li>
   
-            <li class="active-link">
+            <li id="menuId" class="active-link">
               <a href="#">Trocar senha</a>
             </li>
-            <li class="active-link">
+            <li id="menuId" class="active-link">
               <a href="#">Atualização de Dados de Perfil</a>
             </li>
   
