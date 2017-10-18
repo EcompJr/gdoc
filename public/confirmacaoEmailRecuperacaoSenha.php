f4a8b71a8a5c6299d771832e6663b4cfdc7b053c
vvalmeida <vvalmeida96@gmail.com>
2017-10-18 16:55:05 -0300

    Add confirmação email recuperação senha page

---
+++ public/confirmacaoEmailRecuperacaoSenha.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>EGDOC - Confirmação</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/login.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+
+
+    <div id="recoverybox" style="padding-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="panel-title">Confirmação</div>
+            </div>
+            <div class="panel-body">
+                <div clas="row">
+                    <label class="col-md-12">Um email para recuperação da sua senha foi enviado. Verifique sua caixa de entrada ou SPAM.</label>                
+                </div>       
+                    
+                <div class="form-group">
+                    <div class="col-md-offset-5 col-md-9">
+                    <a href="index.php"><button id="btn-recovery" type="submit" class="btn btn-primary">Voltar</button></a>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+    <script src="assets/js/lib/jquery-1.10.2.js"></script>
+
+    <script src="assets/js/lib/bootstrap.min.js"></script>
+
+    <script src="assets/js/lib/validator.min.js"></script>
+
+    <script src="assets/js/estados.js"></script>
+
+    <script src="assets/js/paises.js"></script>
+
+
+
+
+</body>
