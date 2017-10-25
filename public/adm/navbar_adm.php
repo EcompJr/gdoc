99c05e9d90d2b77fce0b215b09b16d42179e7a61
Gustavo Boanerges <g.boanerges2@gmail.com>
2017-10-25 07:27:36 -0300

    Minor adjust navbar

--- public/adm/navbar_adm.php
+++ public/adm/navbar_adm.php
@@ -1,6 +1,6 @@
 <!-- NAVBAR -->
 <div id="wrapper">
-    <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="nav navbar navbar-inverse navbar-fixed-top">
       <div class="adjust-nav">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
@@ -8,12 +8,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="../index.html">
-                <img src="../assets/img/logo.png" />
+            <a class="navbar-left" href="../index.html">
+                <img src="../assets/img/logo.png" alt="Logo da Empresa" widht="50" height="50">
             </a>
         </div>
-        <span class="logout-spn">
-            <a href="#" style="color:#fff;">Sair</a>
-        </span>
+        <!-- Botao para sair quebra linha ao diminuir o tamanho da tela, deveria estar na sidebar
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="#"><span class="glyphicon glyphicon-log-out" style="color: white;"></span>Sair</a>
+          </li>
+        </ul>
+        -->
     </div>
 </div>
--- public/master/navbar_master.php
+++ public/master/navbar_master.php
@@ -3,21 +3,22 @@
     <div class="nav navbar navbar-inverse navbar-fixed-top">
       <div class="adjust-nav">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target=".sidebar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <a class="navbar-left" href="../index.html">
             <img src="../assets/img/logo.png" alt="Logo da Empresa" widht="50" height="50">
           </a>
         </div>
-
+        <!-- Botao para sair quebra linha ao diminuir o tamanho da tela, deveria estar na sidebar
         <ul class="nav navbar-nav navbar-right">
           <li>
             <a href="#"><span class="glyphicon glyphicon-log-out" style="color: white;"></span>Sair</a>
           </li>
         </ul>
+        -->
       </div>
     </div>
 </div>
